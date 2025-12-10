56071c0022f074611ef58d8a3e1d997f8fcefd12
niharadhanesh <niharadhanesh875@gmail.com>
2025-12-10 12:53:21 +0530

    static fix

--- jwellery/settings.py
+++ jwellery/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'app'
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ⭐ REQUIRED FOR RENDER
 ]
 
 ROOT_URLCONF = 'jwellery.urls'
